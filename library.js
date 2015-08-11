94178a14142daa8750f21587a2070ba7f34a9a1b
Darai <daraiyoo@gmail.com>
2015-08-10 23:56:36 -0400

    fixed bugs

---
+++ library.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	
+$("#leftDIV").slideUp
+	(3000, function() {
+      
+        });
+
+
+ $("body").animate({
+          opacity: 0.5
+        }, 3000);
+
+$(".articlegrid3").click(function(){
+	alert("Save the Elep")
+})
+ 
+});
